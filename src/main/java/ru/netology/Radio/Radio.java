838c2fdbc79014c9cd293d3f6dd33947ba383694
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-02-13 22:01:27 +1000

    New branch

--- src/main/java/ru/netology/Radio/Radio.java
+++ src/main/java/ru/netology/Radio/Radio.java
@@ -2,31 +2,62 @@ package ru.netology.Radio;
 
 public class Radio {
 
+    private int maxVolume = 100;
+    private int minVolume = 0;
     private int currentVolume;
     private int currentTrack;
+    private int amount = 10;
+    private int maxTrack = 9;
+    private int minTrack = 0;
+
+
+    public int getMinTrack() {
+        return minTrack;
+    }
+
+    public int getMaxTrack() {
+        return maxTrack;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
+    public int getAmount() {
+        return amount;
+    }
+
+
     public int setCurrentVolume(int currentVolume) {
-        if (currentVolume > 0 & currentVolume <= 10) {
+        if (currentVolume > 0 & currentVolume < 100) {
             this.currentVolume = currentVolume;
             return currentVolume;
         }
-        return currentVolume;
+        return maxVolume;
     }
 
     public void lowerVolume() {
         if (currentVolume > 0) {
-            currentVolume = currentVolume - 1;
+            this.currentVolume = currentVolume - 1;
         }
+
     }
 
-    public void increaseVolume() {
-        if (currentVolume < 10) {
-            currentVolume = currentVolume + 1;
+    public int increaseVolume(int currentVolume) {
+        if (currentVolume < 100) {
+            this.currentVolume = currentVolume + 1;
+            return this.currentVolume;
         }
+        return maxVolume;
+
     }
 
     public int getCurrentTrack() {
@@ -45,18 +76,23 @@ public class Radio {
         if (currentTrack > 0) {
             currentTrack++;
         } else {
-            this.currentTrack = 9;
+            this.currentTrack = getMaxTrack();
         }
     }
-
 
     public void prevTrack() {
         if (currentTrack < 9) {
             currentTrack--;
         } else {
-            this.currentTrack = 0;
+            this.currentTrack = getMinTrack();
         }
+    }
 
+    public Radio(int amount) {
+        this.amount = amount;
+    }
+
+    public Radio() {
     }
 }
 
