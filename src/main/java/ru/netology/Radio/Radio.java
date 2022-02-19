67e162d1eef307b248f5f256fc2570ecf91c90e0
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-02-19 13:16:24 +1000

    lombok

--- src/main/java/ru/netology/Radio/Radio.java
+++ src/main/java/ru/netology/Radio/Radio.java
@@ -1,5 +1,13 @@
 package ru.netology.Radio;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Radio {
 
     private int maxVolume = 100;
@@ -9,103 +17,6 @@ public class Radio {
     private int amount = 10;
     private int maxTrack = 9;
     private int minTrack = 0;
-
-
-    public int getMinTrack() {
-        return minTrack;
-    }
-
-    public int getMaxTrack() {
-        return maxTrack;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-
-    public int setCurrentVolume(int currentVolume) {
-        if (currentVolume > 0 & currentVolume < 100) {
-            this.currentVolume = currentVolume;
-            return currentVolume;
-        }
-        return maxVolume;
-    }
-
-    public void lowerVolume() {
-        if (currentVolume > 0) {
-            this.currentVolume = currentVolume - 1;
-        }
-
-    }
-
-    public int increaseVolume(int currentVolume) {
-        if (currentVolume < 100) {
-            this.currentVolume = currentVolume + 1;
-            return this.currentVolume;
-        }
-        return maxVolume;
-
-    }
-
-    public int getCurrentTrack() {
-        return currentTrack;
-    }
-
-    public void setCurrentTrack(int currentTrack) {
-        if (currentTrack >= 0 & currentTrack <= 9) {
-            this.currentTrack = currentTrack;
-        } else {
-            this.currentTrack = 0;
-        }
-    }
-
-    public void nextTrack() {
-        if (currentTrack > 0) {
-            currentTrack++;
-        } else {
-            this.currentTrack = getMaxTrack();
-        }
-    }
-
-    public void prevTrack() {
-        if (currentTrack < 9) {
-            currentTrack--;
-        } else {
-            this.currentTrack = getMinTrack();
-        }
-    }
-
-    public Radio(int amount) {
-        this.amount = amount;
-    }
-
-    public Radio() {
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
---
+++ src/test/java/ru/netology/Radio/RadioTest.java
@@ -0,0 +1,15 @@
+package ru.netology.Radio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+    @Test
+    void shouldCreate() {
+
+
+        Radio radio = new Radio();
+    }
+    }
+
